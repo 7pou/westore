9d1a9917ad72e171bf7648c1850419fe5c73702b
zhaoyf <604264588@qq.com>
2023-04-21 14:53:48 +0800

    feat: 添加监听, 添加 defineStore

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ const parseData = (store: WeStore) => {
   if (typeof store.data !== 'object') return store.data
   Object.keys(store.data).forEach(key => {
     const v = store.data[key]
-    data[key] = typeof v === 'function' ? v.apply(store) : v
+    data[key] = typeof v === 'function' ? v.call(store, store.data) : v
   })
   return data
 }
@@ -18,9 +18,12 @@ class WeStore {
 
   pages: Map<IInstance, string>
 
+  listeners: Array<(data)=>void>
+
   constructor() {
     this.data = {}
     this.pages = new Map()
+    this.listeners = []
   }
 
   bind(instance: IInstance, name: string) {
@@ -35,11 +38,42 @@ class WeStore {
     this.pages.delete(instance)
   }
 
+  on(fn: ()=> void) {
+    if (this.listeners.find(e => e === fn)) return
+    this.listeners.push(fn)
+  }
+
+  off(fn: () => void) {
+    const index = this.listeners.findIndex(e => e === fn)
+    if (index > -1) {
+      this.listeners.splice(index, 1)
+    }
+  }
+
   update() {
     const data = parseData(this)
     this.pages.forEach((value, instance) => {
       instance.setData({[value]: data})
     })
+    this.listeners.forEach(fn => {
+      if (typeof fn === 'function') fn(data)
+    })
   }
 }
+
+
+const defineStore = (options) => {
+  const store = new WeStore()
+  store.data = options.state || options.data || {}
+  Object.keys(options.getters).forEach(key => {
+    store.data[key] = options.getters[key]
+  })
+  Object.keys(options.actions).forEach(key => {
+    store[key] = options.actions[key]
+  })
+  return store
+}
+
+
+export {defineStore}
 export default WeStore
